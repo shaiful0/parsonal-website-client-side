4510ed26b6b7f56931879d2999ee6be25d8ded8b
sharifulislam <shariful0401@gmail.com>
2022-04-29 02:06:56 +0600

    added home page heder and banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Heder from './component/Home/Header/Heder';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Heder></Heder>
-      
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import perfum from '../../../images/perfum.png'
 
 const Home = () => {
   return (
     <div>
-      <h2>This is home</h2>
+      <img src={perfum} alt="" />
+      <Banner></Banner>
     </div>
   );
 };
